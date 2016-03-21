366f092d294e8d977e8289ced1858b32e9d4af6a
tonikertz <tonikertz@googlemail.com>
2016-03-21 09:51:42 +0100

    Banner und Ausrichtung

--- pacman3/index.php
+++ pacman3/index.php
@@ -16,9 +16,17 @@ if (isset($_GET["size"]) && (!is_nan($_GET["size"])))
 <img src="./grafik/geist.png" id="geist" alt="" style="display: none"/>
 
 <div>
-    <header>
+    <header style="width:<?= $size ?>px;">
         <img src="./grafik/pacman_marque.gif" alt=""/>
     </header>
+    <nav style="width:<?= $size ?>px;">
+        <ul>
+            <li><a href="#">Arcade</a></li>
+            <li><a href="#">Highscore</a></li>
+            <li><a href="#">Einstellungen</a></li>
+            <li><a href="#">Hilfe</a></li>
+        </ul>
+    </nav>
     <section style="width:<?= $size ?>px; height:<?= $size ?>px;">
         <canvas id="spielFeld" width="<?= $size ?>" height="<?= $size ?>">Ihr Browser kann kein HTML 5</canvas>
         <canvas id="pacmanFeld" width="<?= $size ?>" height="<?= $size ?>">Ihr Browser kann kein HTML 5</canvas>

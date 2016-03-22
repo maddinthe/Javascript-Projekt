1c868bb96af40ca9ef571cab8ec80717f374ff50
tonikertz <tonikertz@googlemail.com>
2016-03-22 14:54:17 +0100

    HTML no scroll

--- pacman3/toni.js
+++ pacman3/toni.js
@@ -1,6 +1,4 @@
-/**
- * Created by tkertz on 21.03.2016.
- */
+"use stricht;"
 function show(id) {
     if(document.getElementById) {
         var mydiv = document.getElementById(id);
@@ -9,3 +7,19 @@ function show(id) {
 }
 
 
+function post(){
+    var name = $('#name').val();
+    var alter = $('#alter').val();
+
+    $.post('validate.php',{postname:name,postage:alter},
+    function(daten){
+
+        if(daten=="1"){
+            $('#result').html('Alter als 18');
+        }
+        if(daten=="0"){
+            $('#result').html('Alter unter 18');
+        }
+
+    });
+}
